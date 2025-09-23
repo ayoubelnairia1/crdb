f4e6d22720a88f0348cf937d1de0cd5d77751f76
Ayoub Elnairia <a.elnairia@gmail.com>
2025-09-23 18:32:45 +0200

    Change insurance_number to Char and add bank fields

--- ag_hr_employee/models/models.py
+++ ag_hr_employee/models/models.py
@@ -18,8 +18,11 @@ class HREmployee(models.Model):
     financial_number = fields.Integer(string="Financial Number", required=True, help="Financial number of employee")
     functional_structure_number = fields.Integer(string="Functional Structure Number", required=True, help="Functional structure number of employee")
     social_security_number = fields.Integer(string="Social Security Number", required=True, help="Social security number of employee")
-    insurance_number = fields.Integer(string="Insurance Number", help="Insurance number of employee")
+    insurance_number = fields.Char(string="Insurance Number", size=15, help="Insurance number of employee")
     certificate_info_ids = fields.One2many('hr.certificate.info', 'employee_id', string="Certificate Info")
+    bank_name = fields.Char(string="Bank Name", help="Bank name of employee")
+    bank_account_number = fields.Char(string="Bank Account Number", help="Bank account number of employee")
+    rakam_watany = fields.Char(string="RAKAM_WATANY", size=12, help="National Unique Number (max 12 chars)")
 
 
 class HRCertificateInfo(models.Model):
